8c8701bc3ea3e4e5d5bac8e70ec6e03ada9f23f9
Tornike Japaridze <tornikejaparidze777@gmail.com>
2026-02-24 14:28:28 +0000

    მე მივიღე ერორი პროდუქტის დამტების დროს და პროდუქტის მონაცემები არ აისახ

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3215885165-509b8",
   "appId": "1:227657588522:web:49480468975f6a0d340853",
+  "storageBucket": "studio-3215885165-509b8.appspot.com",
   "apiKey": "AIzaSyALDv4ADRIaSd3aGLcuPDlo-YV4CVNKkkw",
   "authDomain": "studio-3215885165-509b8.firebaseapp.com",
   "measurementId": "",
