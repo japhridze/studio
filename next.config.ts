84217f993e8da77fe8a6fefbcbc62f59b1a041fe
Tornike Japaridze <tornikejaparidze777@gmail.com>
2026-03-21 17:40:11 +0000

    https://www.directdoors.com/cdn/shop/files/coloured-glazed-doors-tri-gla

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.directdoors.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
